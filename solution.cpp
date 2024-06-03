9060483cbe732bd9f2f563602234a0cdb7999cb6
Linca Nicolae-Robert <linca_robert@yahoo.com>
2024-06-03 21:47:09 +0300

    Version 2 direct prompting

--- solution.cpp
+++ solution.cpp
@@ -1,40 +1,30 @@
+int n, m, d;
+cin >> n >> m >> d;
 
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main() {
-    int n, m, k;
-    cin >> n >> m >> k;
-
-    vector<vector<int>> spiders(n, vector<int>(m, 0));
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            char c;
-            cin >> c;
-            if (c != '.') {
-                int direction = 0;
-                if (c == 'L') direction = 1;
-                else if (c == 'R') direction = 2;
-                else if (c == 'U') direction = 3;
-                else if (c == 'D') direction = 4;
-
-                int pos = j;
-                for (int t = 0; t < k; ++t) {
-                    if (i + t >= n) break;
-                    if (direction == 1) pos--;
-                    else if (direction == 2) pos++;
-                    if (pos < 0 || pos >= m) break;
-                    spiders[i + t][pos]++;
-                }
-            }
-        }
-    }
-
-    for (int j = 0; j < m; ++j) {
-        cout << spiders[0][j] << " ";
-    }
-
-    return 0;
+vector<int> c(m);
+for (int i = 0; i < m; ++i) {
+    cin >> c[i];
 }
+
+int currPos = 0;
+vector<int> res(n);
+
+for (int i = 0; i < m; ++i) {
+    if (currPos + d > n) {
+        cout << "NO" << endl;
+        return 0;
+    }
+
+    for (int j = 0; j < c[i]; ++j) {
+        for (int k = 0; k < d && currPos + k < n; ++k) {
+            res[currPos + k] = i + 1;
+        }
+        currPos += d;
+    }
+}
+
+cout << "YES" << endl;
+for (int i = 0; i < n; ++i) {
+    cout << res[i] << " ";
+}
+cout << endl;
