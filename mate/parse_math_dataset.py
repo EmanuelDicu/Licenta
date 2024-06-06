46cf199f8b6d8d1ee5a38ebd42cbade0e49e2f98
Linca Nicolae-Robert <linca_robert@yahoo.com>
2024-06-06 16:24:28 +0300

    Add math propting

---
+++ mate/parse_math_dataset.py
@@ -0,0 +1,59 @@
+import os
+import json
+
+
+dataset_directory = 'problems_dataset'
+math_directory = 'MATH'
+
+categories = ['Algebra', 'Counting & Probability', 'Geometry', 'Number Theory', 'Intermediate Algebra', 'Prealgebra', 'Precalculus']
+levels = ['Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5']
+number_of_problems = {category: {level: 0 for level in levels} for category in categories}
+max_number_of_problems_per_level = 2
+
+
+# Function to parse the math dataset
+def parse_math_dataset(base_directory):
+
+    # Walk through all directories and files
+    for root, dirs, files in os.walk(base_directory):
+        for file in files:
+            if file.endswith('.json'):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as f:
+                    data = json.load(f)
+                
+                problem = data['problem']
+                category = data['type']
+                level = data['level']
+                solution = data['solution']
+
+                if category not in categories:
+                    continue
+                if level not in levels:
+                    continue
+
+                # Check if the number of problems for the category and level is less than the maximum
+                if number_of_problems[category][level] < max_number_of_problems_per_level:
+                    # Add file to dataset_directory with the problem, category, level, and solution
+                    json_data = {
+                        'problem': problem,
+                        'category': category,
+                        'level': level,
+                        'solution': solution
+                    }
+
+                    # write the json data to a file
+                    filename = f'{category}_{level}_{number_of_problems[category][level] + 1}.json'
+                    with open(os.path.join(dataset_directory, filename), 'w') as f:
+                        json.dump(json_data, f, indent=4)
+
+                    # Increment the number of problems for the category and level
+                    number_of_problems[category][level] += 1
+            
+            if all(number_of_problems[category][level] == max_number_of_problems_per_level for category in categories for level in levels):
+                return
+
+# Parse the math dataset
+parse_math_dataset(math_directory)
+
+
